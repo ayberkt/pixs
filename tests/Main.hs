89295f9b93f84ea612bf4d9475e15f0d4a6fbbde
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-12 01:58:33 -0600

    Trying to make the Arbitrary instance work

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving #-}
 
 module Main where
 import Test.Hspec
 import Test.QuickCheck
 import Data.Word (Word8)
 import Codec.Picture (PixelRGBA8(..), Image(..), pixelAt)
+import Control.Applicative
 import qualified Transformations as T
 import qualified Data.Vector.Storable as VS
 
@@ -13,33 +14,46 @@ instance Arbitrary (Image PixelRGBA8) where
     l <- listOf (arbitrary :: Gen Word8)
     w <- (arbitrary :: Gen Int)
     h <- (arbitrary :: Gen Int)
-    return $ Image { imageWidth = w
+    return $ Image { imageWidth  = w
                    , imageHeight = h
-                   , imageData = VS.fromList l}
+                   , imageData   = VS.fromList l
+                   }
 
 instance Eq (Image PixelRGBA8) where
   img1 == img2
+    | (width1 == 0) && (width2 == 0)
+      = True
+    | (height1 == 0) && (height2 == 0)
+      = True
     | width1 /= width2
       = False
     | height1 /= height2
       = False
-    | otherwise = all id [foo x y | x <- [0.. width1-1]
-                                  , y <- [0.. height1-1]]
+    | otherwise = all id [(foo img1 img2 x y) | x <- [0..width1]
+                                              , y <- [0..height1]]
     where width1  = imageWidth  img1
           width2  = imageWidth  img2
           height1 = imageHeight img1
           height2 = imageHeight img2
-          foo x y = (pixelAt img1 x y) == (pixelAt img2 x y)
+          foo p q x y = (pixelAt p x y) == (pixelAt q x y)
 
 instance Show (Image PixelRGBA8) where
   show _ = ""
 
+reflexivity :: Image PixelRGBA8 -> Bool
+reflexivity img = img == img
 
 doubleFlipIsID :: Image PixelRGBA8 -> Bool
 doubleFlipIsID img = (T.flip (T.flip  img)) == img
 
+plusCommutative :: Int -> Int -> Bool
+plusCommutative x y = x + y == y + x
+
 main :: IO ()
 main = hspec $ do
-  describe "Double flip" $ do
-    it "doesn't modify the image" $ property $
-      doubleFlipIsID
+  describe "Image equality" $ do
+    it "is reflexive" $ property $
+      reflexivity
+  describe "Addition operation" $ do
+    it "is commutative" $ property $
+      plusCommutative
