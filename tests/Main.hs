e5d498f0d966fbd065512bd260dfa130373529bc
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-11 20:14:54 -0600

    AddSetup for tests

---
+++ tests/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+import Test.Hspec
+import Test.QuickCheck
+
+plusCommutative :: Int -> Int -> Bool
+plusCommutative x y = x + y == y + x
+
+main :: IO ()
+main = hspec $ do
+  describe "Plus" $ do
+    it "-> is commutative" $ property $
+      plusCommutative
