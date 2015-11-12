2c062909d186d00417c5a002685f9d618331d6b8
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-11 20:49:59 -0600

    Make Image PixelRGBA8 an instance of arbitrary

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,13 +1,26 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 import Test.Hspec
 import Test.QuickCheck
 import Transformations
+import Data.Word (Word8)
+import Codec.Picture (PixelRGBA8(..), Image(..))
+import qualified Data.Vector.Storable as VS
 
-plusCommutative :: Int -> Int -> Bool
-plusCommutative x y = x + y == y + x
+instance Arbitrary (Image PixelRGBA8) where
+  arbitrary = do
+    l <- listOf (arbitrary :: Gen Word8)
+    w <- (arbitrary :: Gen Int)
+    h <- (arbitrary :: Gen Int)
+    return $ Image { imageWidth = w
+                   , imageHeight = h
+                   , imageData = VS.fromList l}
+
+
 
 main :: IO ()
 main = hspec $ do
   describe "Plus" $ do
     it "-> is commutative" $ property $
-      plusCommutative
+      False
