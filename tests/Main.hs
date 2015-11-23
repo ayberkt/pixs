4bde452c3e203b82e6c6bd867b32ca4dd487cbd3
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-23 16:08:31 -0600

    Image arbitrary instance fixed

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE  FlexibleInstances
-            , GeneralizedNewtypeDeriving
-            , StandaloneDeriving #-}
+{-# LANGUAGE  FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UnicodeSyntax #-}
 
 module Main where
 import Test.Hspec
@@ -12,15 +13,29 @@ import Codec.Picture ( PixelRGBA8(..)
                      , PixelBaseComponent(..))
 import qualified Transformations as T
 import qualified Data.Vector.Storable as VS
+import Control.Monad (replicateM)
 
 instance Arbitrary (Image PixelRGBA8) where
   arbitrary = do
-    l          <- listOf (arbitrary :: Gen Word8)
-    Positive w <- (arbitrary :: Gen (Positive Int))
-    return $ Image { imageWidth  = w
-                   , imageHeight = ((length l) `div` w `div` 4)
-                   , imageData   = VS.fromList l
+    Positive size ← (arbitrary ∷ Gen (Positive Int))
+    pixs          ← listOfSize size
+    Positive w    ← arbitrary ∷ Gen (Positive Int)
+    let w' = w `rem` size-1
+    return $ Image { imageWidth  = w'
+                   , imageHeight = w' `div` size-1
+                   , imageData   = VS.fromList pixs
                    }
+
+listOfSize ∷ Int → Gen [Word8]
+listOfSize x = fmap concat $ replicateM x genPixel
+
+genPixel ∷ Gen [Word8]
+genPixel = do
+     a ← arbitrary ∷ Gen Word8
+     b ← arbitrary ∷ Gen Word8
+     c ← arbitrary ∷ Gen Word8
+     d ← arbitrary ∷ Gen Word8
+     return [a,b,c,d]
 
 
 deriving instance Eq (Image PixelRGBA8)
@@ -48,11 +63,11 @@ deriving instance Show (Image PixelRGBA8)
 --           height2 = imageHeight img2
 --           foo p q x y = (pixelAt p x y) == (pixelAt q x y)
 
-reflexivity :: Image PixelRGBA8 -> Bool
-reflexivity img = img == img
+prop_reflexivity :: Image PixelRGBA8 -> Bool
+prop_reflexivity img = img == img
 
-doubleFlipIsID :: Image PixelRGBA8 -> Bool
-doubleFlipIsID img = if    (imageWidth img) >= 1
+prop_double_flip_ID :: Image PixelRGBA8 -> Bool
+prop_double_flip_ID img = if    (imageWidth img) >= 1
                         && (imageHeight img) >= 1
                         && (pixsLength `rem` 4) == 0
                         then (T.flipVertical (T.flipVertical  img)) == img
@@ -66,7 +81,7 @@ main :: IO ()
 main = hspec $ do
   describe "Image equality" $ do
     it "is reflexive" $ property $
-      reflexivity
+      prop_reflexivity
   describe "flipVertical" $ do
     it "gives identity when applied twice" $ property $
-      doubleFlipIsID
+      prop_double_flip_ID
