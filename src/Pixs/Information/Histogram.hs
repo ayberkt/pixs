0198128119c1f443de62f51baa9ab8ecb0a743bd
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-12-25 06:01:48 +0200

    Add function for counting blue

--- src/Pixs/Information/Histogram.hs
+++ src/Pixs/Information/Histogram.hs
@@ -2,9 +2,14 @@
 
 module Pixs.Information.Histogram where
 
-import           Codec.Picture (Image, PixelRGBA8 (..), imageHeight, imageWidth,
-                                pixelAt)
-import           Data.Map      (Map, adjust, fromList)
+import           Codec.Picture ( Image
+                               , PixelRGBA8(..)
+                               , imageHeight
+                               , imageWidth
+                               , pixelAt)
+import           Data.Map      ( Map
+                               , adjust
+                               , fromList)
 import           Data.Word     (Word8)
 import           Prelude       hiding (lookup)
 
@@ -18,3 +23,14 @@ redCount img = let count ∷ Map Word8 Int → Int → Int → Map Word8 Int
                            m' = adjust (+ 1) r' m
                    initMap = fromList $ map (\x → (x, 0)) [0..255]
                in count initMap 0 0
+
+blueCount ∷ Image PixelRGBA8 → Map Word8 Int
+blueCount img = let count ∷ Map Word8 Int → Int → Int → Map Word8 Int
+                    count m x y
+                      | y == imageHeight img - 1 = m
+                      | x == imageWidth  img - 1 = count m' 0 (succ y)
+                      | otherwise = count m' (succ x) y
+                      where r' = case pixelAt img x y of (PixelRGBA8 _ _ b _) → b
+                            m' = adjust (+ 1) r' m
+                    initMap = fromList $ map (\x → (x, 0)) [0..255]
+                in count initMap 0 0
